463f6ca8dc21e4d30cc8ac2fccbd0c94f096cfbf
devansh.jain <devansh.jain@G1-BWXRM13-L>
2024-06-13 16:46:27 +0530

    added section 4

---
+++ Section 4 - Deep Dive into React Essentials/src/components/Player.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing); // Schedule a state update
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            className="player-name"
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
